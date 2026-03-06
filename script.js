15c4bec432b5e36681ffa5317a6cebc3e290ffe9
Renier Souza <renier@objetivonet.com.br>
2026-03-06 15:48:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ alert("Preencha todos os campos")
 return
 }
 
+if(quantidade < 5){
+alert("Estoque baixo!")
+}
+
 let lista = document.getElementById("lista")
 
 let item = document.createElement("li")
@@ -20,18 +24,23 @@ lista.appendChild(item)
 document.getElementById("produto").value = ""
 document.getElementById("quantidade").value = ""
 
+atualizarContador()
+
 }
 
 function removerProduto(botao){
 
 botao.parentElement.remove()
 
+atualizarContador()
+
 }
 
 function atualizarContador(){
+
 let total = document.querySelectorAll("#lista li").length
-document.getElementById("contador").textContent = "Total de produtos: " + total
-}
-if(quantidade < 5){
-alert("Estoque baixo!")
+
+document.getElementById("contador").textContent =
+"Total de produtos: " + total
+
 }
